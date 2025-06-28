c5f18f8df6d39bb9bba77129e0717ee53bf5d882
Srikanth <kumarsrikanth050595@gmail.com>
2025-06-28 18:10:52 +0530

    Update project

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,10 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
+          <div>
+
           <Hero />
+          </div>
         </div>
         <About />
         <Experience />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,10 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto bg-primary">
+    <section className="relative w-full h-screen mx-auto">
       <div
-  className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 z-20`}
->
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
@@ -16,7 +16,7 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915eff]">Srikanth</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-[#915eff]`}>
+          <p className={`${styles.heroSubText} mt-2 text-[915eff]`}>
             I'm a passionate{" "}
             <span className="text-white">
               Full Stack <br className="sm:block hidden" />
@@ -24,8 +24,9 @@ const Hero = () => {
             </span>
           </p>
         </div>
-      </div>
+    
       <ComputersCanvas />
+        </div>
 
       <div
         className="absolute xs:bottom-0
