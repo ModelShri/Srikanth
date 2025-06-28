4ec89a78261473af203377f2dc314e1b15c7acb7
Srikanth <kumarsrikanth050595@gmail.com>
2025-06-28 17:56:45 +0530

    Fix hero section for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div
         className="absolute xs:bottom-0
