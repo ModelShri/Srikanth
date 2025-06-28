efdb8d6606fd8a4987d942854138ce16c7dc62f0
Srikanth <kumarsrikanth050595@gmail.com>
2025-06-28 18:00:50 +0530

    Fix hero section for mobile

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
-  plugins: [react()]
-};
+export default defineConfig({
+  base: '/srikanth_gaddale/',
+  plugins: [react()],
+});
