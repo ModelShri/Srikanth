72827aab7459fe239021b3ed6eb996fc44abc667
Srikanth <kumarsrikanth050595@gmail.com>
2026-08-01 12:45:52 +0530

    fix: remove Preload all from ComputersCanvas to prevent WebGL premature shader compile crash at 97% loading progress

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
@@ -8,8 +8,8 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.65} groundColor="black" />
-      <ambientLight intensity={0.75} />
+      <hemisphereLight intensity={0.75} groundColor="black" />
+      <ambientLight intensity={0.85} />
       <pointLight intensity={1.5} position={[0, 10, 0]} />
       <spotLight
         position={[-20, 50, 10]}
@@ -19,7 +19,7 @@ const Computers = ({ isMobile }) => {
       />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 0.65 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -53,6 +53,8 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true, antialias: true }}
       className="w-full h-full"
     >
+      <color attach="background" args={["#050816"]} />
+
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
@@ -61,10 +63,10 @@ const ComputersCanvas = () => {
         />
         <Computers isMobile={isMobile} />
       </Suspense>
-
-      <Preload all />
     </Canvas>
   );
 };
+
+useGLTF.preload("/desktop_pc/scene.gltf");
 
 export default ComputersCanvas;
